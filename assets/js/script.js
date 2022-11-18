f1f9522e6f99e5ade905923f1ab30bcc24f482d2
rubymaxioned <axioned0482022>
2022-11-18 15:07:14 +0530

    Enter final

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,77 +1,37 @@
-// var input = document.querySelector(".input-text");
-// var mainLi = document.querySelector("ul")
-// var list = document.querySelectorAll("li");
-// console.log(list);
-// var inputVal = document.querySelector(".input-text").value;
-// console.log(input.value);
-// input.addEventListener('input', function(event) {
-    // console.log(event.target.value);
-    // var getInput = event.target.value;
-        // console.log(getInput);
-        // var separatedInput = getInput.split(',');
-        // console.log(separatedInput);
-    //     if (event.keyCode == 13) {
-    //     }
-    // input.addEventListener("keydown", myFunction);
-    // function myFunction(e) {
-    //     // keySpan.innerText = e.key;
-    //     // codeSpan.innerText = e.keyCode;
-    //     // keycodeSpan.innerText = e.code;
-    //     // a.classList.add('hide');
-    //     // b.classList.add('show');
-    //     // console.log(e);
-    //         if (e.keyCode == 13) {
-    //     // console.log(e.keyCode);
-    //     var getInput = event.target.value;
-    //     var separatedInput = getInput.split(',');
-    //     console.log(separatedInput);
-    // }
-    // }
+//     var input = document.querySelector(".input-text");
+//     var mainLi = document.querySelector("ul")
+//     // var list = document.querySelectorAll("li");
+//     console.log("initial list");
+   
+//     // console.log(list);
     
-    // if (event.target.code == 13) {
-    //     console.log(event.code);
-    //    
-    // })
-
-    // console.log(separatedInput.length);
-    // for(i=0; i<separatedInput.length; i++){
-    //     list[i].innerText = separatedInput[i].value;
-    // }
-        // console.log(separatedInput);
-
-
-    var input = document.querySelector(".input-text");
-    var mainLi = document.querySelector("ul")
-    // var list = document.querySelectorAll("li");
-    console.log("initial list");
-    // console.log(list);
-    
-    var updatedList = [];
-    input.addEventListener('keyup', function(event) {
-        if(event.key == 'Enter') {
-            var getInput = event.target.value;
-            var separatedInput = getInput.split(',');
-            
+//     var updatedList = [];
+//     input.addEventListener('keyup', function(event) {
+//         if(event.key == ',') {
+//             var getInput = event.target.value;
+//             // var getInputFinal = [];
+//             getInputString = getInput.substring(0,getInput.length-1);
+//             // getInputFinal.push(getInputString);
+//             // console.log(getInputFinal.length);
                 
-                for(i=0; i<separatedInput.length; i++){
-                    var list = document.createElement('li');
-                    list.classList.add('some');
-                    mainLi.append(list);
-                    list.innerText = separatedInput[i];
-                    updatedList.push(list);
-                }
-                console.log("updated list");
-                console.log(updatedList);
-                console.log(updatedList.length);
-                var ranVal = updatedList[Math.floor(Math.random()*updatedList.length)];
-                console.log(ranVal);
-                ranVal.classList.add('pick');
-            event.target.value = '';
-            // randomSelect();
-        }
-    })
+//                 // for(i=0; i<getInputFinal.length; i++){
+//                     var list = document.createElement('li');
+//                     // list.classList.add('some');
+//                     mainLi.append(list);
+//                     list.innerText = this.value;
+//                     updatedList.push(list);
+//                 // }
 
-console.log(updatedList);
+//             //     var ranVal = updatedList[Math.floor(Math.random()*updatedList.length)];
+//             //     console.log(ranVal);
+//             //     ranVal.classList.add('pick');
+//             // event.target.value = '';
+//             // randomSelect();
+//         }
+//     })
+
+// console.log(updatedList);
+// console.log(getInputFinal);
     // function randomSelect() {
     //     var times = 30;
     //     var interval = setInterval(function () {
@@ -105,3 +65,184 @@ console.log(updatedList);
     // function unHighLight(ran){
     //     ran.classList.remove('pick');
     // }
+
+     // console.log("getInputString");
+            // console.log(getInputString);
+            // console.log(getInputString.length);
+            // var separatedInput = getInput.split(',');
+            // console.log(separatedInput);
+
+
+
+//             var input = document.querySelector(".input-text");
+//     var mainLi = document.querySelector("ul")
+//     // var list = document.querySelectorAll("li");
+//     console.log("initial list");
+//     // console.log(list);
+    
+//     var updatedList = [];
+//     input.addEventListener('keyup', function(event) {
+//         if(event.key == ',') {
+//             var getInput = this.value;
+//             var separatedInput = getInput.trim().split(',');
+//             console.log(separatedInput);
+                
+//                 // for(i=0; i<separatedInput.length; i++){
+//                     var list = document.createElement('li');
+//                     // list.classList.add('some');
+//                     mainLi.append(list);
+//                     list.innerText = separatedInput;
+//                     updatedList.push(list);
+//                 // }
+//             //     console.log("updated list");
+//             //     console.log(updatedList);
+//             //     console.log(updatedList.length);
+//             //     var ranVal = updatedList[Math.floor(Math.random()*updatedList.length)];
+//             //     console.log(ranVal);
+//             //     ranVal.classList.add('pick');
+//             // event.target.value = '';
+//             // randomSelect();
+//         }
+//     })
+
+// console.log(updatedList);
+
+var input = document.querySelector(".input-text");
+    // var mainLi = document.querySelector("ul");
+    // var updatedList = [];
+
+    // input.addEventListener('keyup', function(event) {
+    //     var current = this.value.trim().split(',');
+    //     console.log(current);
+    //     var list = document.createElement('li');
+    //                         // list.classList.add('some');
+    //                         mainLi.append(list);
+    //                         list.innerText = current;
+    //                         updatedList.push(list);
+    // })
+
+
+
+  
+    var input = document.querySelector(".input-text");
+    var mainLi = document.querySelector("ul")
+    // var list = document.querySelectorAll("li");
+    // console.log(list);
+    var counter = 0;
+    
+    var updatedList = [];
+    input.addEventListener('keyup', function(event) {
+
+        // if(event.key == 'Enter') {
+        // }
+        if(event.key == 'Enter') {
+            var getInput = event.target.value;
+            var separatedInput = getInput.split(',');
+            
+            
+            for(i=0; i<separatedInput.length; i++){
+                var list = document.createElement('li');
+                // list.classList.add('some');
+                    mainLi.append(list);
+                    list.innerText = separatedInput[i];
+                    updatedList.push(list);
+                }
+                
+                // console.log("updated list");
+                // console.log(updatedList);
+                // console.log(updatedList.length);
+                // console.log(ranVal);
+                event.target.value = '';
+                // randomSelect();
+                var interval = setInterval(function(){
+                    console.log(Math.floor(Math.random()*updatedList.length));
+                    var ranVal = updatedList[Math.floor(Math.random()*updatedList.length)];
+                    ranVal.classList.add('pick');
+                    // console.log(ranVal);
+                    setTimeout(function(){
+                        ranVal.classList.remove('pick');
+                    },100);
+                    // clearInterval(interval);
+                    if(counter == updatedList.length*2){
+                        // if(ranVal.classList.contains('pick')) {
+                            // console.log(ranVal);
+                            clearInterval(interval);
+                            setTimeout(function(){
+                                ranVal.classList.add('pick');
+                            },100);
+                        // }
+                    }
+                    counter++;
+                },200);
+            }
+            
+        })
+        
+        
+
+        // function randomSelect() {
+        //             var times = 30;
+        //             var interval = setInterval(function() {
+        //             // var random = randomList();
+        //             // console.log("random");
+        //             // console.log(random)
+            
+        //             // console.log(random);
+
+        //             console.log(ranVal);
+        //             highLight(ranVal);
+        //             setTimeout(() => {
+        //                 unHighLight(ranVal);
+        //             },300);
+        //                 clearInterval(interval);
+        //                 },300);
+        //         }
+        
+        //         function highLight(ran){
+        //                     ranVal.classList.add('pick');
+        //                 }
+                    
+        //                 function unHighLight(ran){
+        //                     ranVal.classList.remove('pick');
+        //                 } 
+
+
+//         console.log("initial list");
+//         console.log(updatedList);
+
+//         function randomSelect() {
+//         var times = 30;
+//         var interval = setInterval(function() {
+//         var random = randomList();
+//         console.log("random");
+//         console.log(random)
+
+//         // console.log(random);
+//         highLight(random);
+//         setTimeout(() => {
+//             unHighLight(random);
+//         },300);
+//             clearInterval(interval);
+//             },300);
+//     }
+
+
+// function randomList(){
+//         // console.log("updated list");
+//         // console.log(updatedList);
+//         // var ranLi = document.querySelectorAll("mainLi li");
+//         // console.log("ranli");
+//         // console.log(ranLi);
+//             updatedList[Math.floor(Math.random()*updatedList.length)];
+//             // console.log(ranLi[Math.floor(Math.random()*ranLi.length)]);
+//             // console.log("ranVal");
+//             // console.log(ranVal);
+//     }
+
+//     function highLight(ran){
+//         ranVal.classList.add('pick');
+//     }
+
+//     function unHighLight(ran){
+//         ranVal.classList.remove('pick');
+//     }
