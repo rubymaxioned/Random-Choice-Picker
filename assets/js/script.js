6c05dcf53743ffbedd515ce14b94e9cc6fd26550
rubymaxioned <axioned0482022>
2022-11-18 11:51:01 +0530

    added random

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,67 +42,66 @@
 
     var input = document.querySelector(".input-text");
     var mainLi = document.querySelector("ul")
-    var list = document.querySelectorAll("li");
+    // var list = document.querySelectorAll("li");
     console.log("initial list");
-    console.log(list);
+    // console.log(list);
     
-
+    var updatedList = [];
     input.addEventListener('keyup', function(event) {
         if(event.key == 'Enter') {
             var getInput = event.target.value;
             var separatedInput = getInput.split(',');
-
-            var li = '';
-            separatedInput.forEach(function(item) {
-                // console.log(item);
-                li += "<li>" + item + "</li>";
-                // console.log(li);
-                randomSelect();
-            })
-
-         
-            mainLi.innerHTML = li;
-            // console.log(mainLi.innerHTML);
+            
+                
+                for(i=0; i<separatedInput.length; i++){
+                    var list = document.createElement('li');
+                    list.classList.add('some');
+                    mainLi.append(list);
+                    list.innerText = separatedInput[i];
+                    updatedList.push(list);
+                }
+                console.log("updated list");
+                console.log(updatedList);
+                console.log(updatedList.length);
+                var ranVal = updatedList[Math.floor(Math.random()*updatedList.length)];
+                console.log(ranVal);
+                ranVal.classList.add('pick');
             event.target.value = '';
-            // var interval = setInterval(function () {
-            //     separatedInput.forEach(function(item) {
-            //         // li.classList.add('pick');
-            //     })
-            // // clearInterval(interval);
-            // },1000);
-
+            // randomSelect();
         }
     })
 
-    function randomSelect() {
-        var times = 30;
-        var interval = setInterval(function () {
-        var random = randomList();
-        // console.log(random);
-        highLight(random);
-        setTimeout(() => {
-            unHighLight(random);
-        },300);
-            clearInterval(interval);
-            },300);
-    }
+console.log(updatedList);
+    // function randomSelect() {
+    //     var times = 30;
+    //     var interval = setInterval(function () {
+    //     var random = randomList();
+    //     console.log("random value");
 
-    function randomList(){
-        // list.forEach(function(count){
-            console.log("updated list");
-            console.log(list);
-            console.log("working");
-            // list.classList.add('pick');
-            console.log(list[Math.floor(Math.random()*list.length)]);
-            console.log(list);
-        // })
-    }
-    // console.log(list);
+    //     console.log(random);
+    //     highLight(random);
+    //     setTimeout(() => {
+    //         unHighLight(random);
+    //     },300);
+    //         clearInterval(interval);
+    //         },300);
+    // }
 
-    function highLight(ran){
-        ran.classList.add('pick');
-    }
+    // function randomList(){
+    //     // console.log("updated list");
+    //     // console.log(updatedList);
+    //     var ranLi = document.querySelectorAll("mainLi li");
+    //     console.log("ranli");
+    //     console.log(ranLi);
+    //         var ranVal = ranLi[Math.floor(Math.random()*ranLi.length)];
+    //         // console.log(ranLi[Math.floor(Math.random()*ranLi.length)]);
+    //         console.log(ranVal);
+    // }
 
-    function unHighLight(ran){
-        ran.classList.remove('pick');
-    }
+    // function highLight(ran){
+    //     ran.classList.add('pick');
+    // }
+
+    // function unHighLight(ran){
+    //     ran.classList.remove('pick');
+    // }
