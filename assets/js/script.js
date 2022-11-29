667dc8b3693f62763d6f50cf6701eb148dad3a35
rubymaxioned <axioned0482022>
2022-11-29 16:53:58 +0530

    getting input as enter

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,32 +3,28 @@ var input = document.querySelector(".input-text");
 var mainLi = document.querySelector("ul");
 
 var counter = 0;
-var updatedList = [];
-
-function orangeButton(some) {
-    var commaSeparatedInput = some.split(',');
-    var list = document.createElement('li');
-    mainLi.append(list);
-    list.innerText = commaSeparatedInput.pop();
-    updatedList.push(list);
-}
+// var updatedList = [];
 
 input.addEventListener('keyup', function (event) {
+    mainLi.innerHTML = "";
     var getInput = event.target.value;
-    if (event.key == ',') {
-        getInputString = getInput.slice(0, getInput.length - 1);
-        orangeButton(getInputString);
-    }
-
-    if (event.key == 'Enter') {
-        var separatedInput = getInput.split(',');
+    var getInputString = getInput.trim().split(',');
+    // str = str.replace(/,\s*$/, "");
+    console.log(getInputString);
+    getInputString.forEach(function(index){
+        console.log(index);
         var list = document.createElement('li');
         mainLi.append(list);
-        list.innerText = separatedInput[separatedInput.length - 1];
-        updatedList.push(list);
-        event.target.value = '';
+        list.innerText = index;  
+    })
+    var updatedList = getInputString;  
+    
+    if (event.key == 'Enter') {
+        // console.log(mainLi);
+        // console.log(mainLi.children.length)
         var interval = setInterval(function () {
-            var ranVal = updatedList[Math.floor(Math.random() * updatedList.length)];
+            var ranVal = mainLi.children[Math.floor(Math.random() * mainLi.children.length)];
+            console.log(ranVal);
             ranVal.classList.add('pick');
             setTimeout(function () {
                 ranVal.classList.remove('pick');
@@ -43,5 +39,49 @@ input.addEventListener('keyup', function (event) {
             counter++;
         }, 200);
     }
-
 })
+
+
+// var counter = 0;
+// var updatedList = [];
+
+// function orangeButton(some) {
+//     var commaSeparatedInput = some.split(',');
+//     var list = document.createElement('li');
+//     mainLi.append(list);
+//     list.innerText = commaSeparatedInput.pop();
+//     updatedList.push(list);
+// }
+
+// input.addEventListener('keyup', function (event) {
+//     var getInput = event.target.value;
+//     if (event.key == ',') {
+//         getInputString = getInput.slice(0, getInput.length - 1);
+//         orangeButton(getInputString);
+//     }
+
+//     if (event.key == 'Enter') {
+//         var separatedInput = getInput.split(',');
+//         var list = document.createElement('li');
+//         mainLi.append(list);
+//         list.innerText = separatedInput[separatedInput.length - 1];
+//         updatedList.push(list);
+//         event.target.value = '';
+//         var interval = setInterval(function () {
+//             var ranVal = updatedList[Math.floor(Math.random() * updatedList.length)];
+//             ranVal.classList.add('pick');
+//             setTimeout(function () {
+//                 ranVal.classList.remove('pick');
+//             }, 100);
+//             if (counter == updatedList.length * 2) {
+//                 clearInterval(interval);
+//                 setTimeout(function () {
+//                     ranVal.classList.add('pick');
+//                 }, 100);
+//             }
+
+//             counter++;
+//         }, 200);
+//     }
+
+// })
