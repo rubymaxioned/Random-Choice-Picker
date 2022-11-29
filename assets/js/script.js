24d0d76fa32723c6301a51f5a31598c49c962989
rubymaxioned <axioned0482022>
2022-11-29 19:31:06 +0530

    final

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,28 @@
 
-var input = document.querySelector(".input-text");
-var mainLi = document.querySelector("ul");
-var counter = 0;
+var input = document.querySelector(".input-text"),
+    mainLi = document.querySelector("ul"),
+    counter = 0;
 
 input.addEventListener('keyup', function (event) {
     mainLi.innerHTML = "";
-    var getInput = event.target.value;
-    var getInputString = getInput.trim().split(',');
-    var lastChar = getInputString.slice(-1);
-    if (lastChar == '') { 
+    var getInput = event.target.value,
+        getInputString = getInput.trim().split(','),
+        lastChar = getInputString.slice(-1);
+    
+        if (lastChar == '') { 
         getInputString = getInputString.slice(0, -1); // trim last character
-    }
+        }
     
     getInputString.forEach(function(index){
         var list = document.createElement('li');
         list.innerText = index;  
         mainLi.append(list);
     })
+    
     var updatedList = getInputString;  
     
     if (event.key == 'Enter') {
-        input.innerText = "";
+        input.value = "";
         var interval = setInterval(function () {
             var ranVal = mainLi.children[Math.floor(Math.random() * mainLi.children.length)];
             ranVal.classList.add('pick');
